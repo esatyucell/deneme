0b1148c2598071f5ebf371e419fc95d579ad03ea
Mustafa Esat Yücel <esatyucelsoftware@gmail.com>
2026-09-13 20:49:27 +0300

    Update memories.js

--- src/data/memories.js
+++ src/data/memories.js
@@ -1,37 +1,37 @@
 export const memories = [
   {
     id: 1,
-    image: "/images/image1.jpeg",
+    image: "/deneme/images/image1.jpeg",
     text: "Seninle geçen her an biraz daha güzel."
   },
   {
     id: 2,
-    image: "/images/image2.jpeg",
+    image: "/deneme/images/image2.jpeg",
     text: "İyi ki yollarımız kesişmiş."
   },
   {
     id: 3,
-    image: "/images/image3.jpeg",
+    image: "/deneme/images/image3.jpeg",
     text: "Senin yanında zamanın nasıl geçtiğini unutuyorum."
   },
   {
     id: 4,
-    image: "/images/image4.jpeg",
+    image: "/deneme/images/image4.jpeg",
     text: "En sevdiğim anılarımın içinde hep sen varsın."
   },
   {
     id: 5,
-    image: "/images/image5.jpeg",
+    image: "/deneme/images/image5.jpeg",
     text: "Birlikte daha nice güzel anılara."
   },
   {
     id: 6,
-    image: "/images/image6.jpeg",
+    image: "/deneme/images/image6.jpeg",
     text: "Gülüşünü her gördüğümde yeniden mutlu oluyorum."
   },
   {
     id: 7,
-    image: "public/images/image7.jpeg",
+    image: "/denemepublic/images/image7.jpeg",
     text: "İyi ki benimsin. ❤️"
   }
 ];
